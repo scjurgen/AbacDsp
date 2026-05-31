dfe0c78aff08f928f79b0028a7faa95c300402bc
Jürgen Schwietering <scjurgen@yahoo.com>
2026-05-31 11:23:36 +0200

    fix WaveformGauge label drawn in red (leftover debug colour)

--- JuceStandaloneGenerator/templates/sourcefiles/inc/WaveformMeter.h
+++ JuceStandaloneGenerator/templates/sourcefiles/inc/WaveformMeter.h
@@ -97,7 +97,7 @@ class WaveformGauge : public juce::Component
     {
         g.setColour(backgroundDarkGrey);
         g.fillRoundedRectangle(getLocalBounds().toFloat(), 3);
-        g.setColour(juce::Colours::red);
+        g.setColour(juce::Colour(GuiConstants::instance().colors.statusOutline));
         g.drawText(m_label, getLocalBounds().removeFromTop(20), juce::Justification::centred);
     }
 
